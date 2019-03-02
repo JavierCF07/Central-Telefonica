d40f7ec6d385eed6b378799e02ebe16fe5547cd3
JavierCF07 <33670804+JavierCF07@users.noreply.github.com>
2019-03-01 21:16:49 -0600

    Iniciando Proyecto

---
+++ Entidades/Llamada.cs
@@ -0,0 +1,31 @@
+namespace CentralTelefonica.Entidades
+{
+    public abstract class Llamada
+    {
+        private string numeroOrigen;
+        public string NumeroOrigen
+        {
+            get { return numeroOrigen;}
+            set { numeroOrigen = value;}
+        }
+
+        private string numeroDestino;
+        public string NumeroDestino
+        {
+            get { return numeroDestino;}
+            set { numeroDestino = value;}
+        }
+        
+        private double duracion;
+        public double Duracion
+        {
+            get { return duracion;}
+            set { duracion = value;}
+        }
+        
+        public abstract double CalcularPrecio();
+        public override string ToString(){
+            return $"Numero de Origen: {this.numeroOrigen} - Numero de Destino: {this.numeroDestino} - Duracion llamada: {this.duracion}";
+        }
+    }
+}
